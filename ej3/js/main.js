0db140b1f97d4ffe4b42961c0f82f9c16d440881
cristinatato <c.tatodelatorre@gmail.com>
2019-07-31 09:20:26 +0200

    add ej3

--- ej3/js/main.js
+++ ej3/js/main.js
@@ -1,12 +1,24 @@
 'use strict';
 
-let numbers = [];
+const lostNumbers = [4, 8, 15, 16, 42];
+let evenNumbers = [];
+let multNumbers = [];
+let concatNumbers = [];
 
-function get100Numbers () {
-    for (let i = 0; i< 100; i++) {
-        console.log (i+1);
-        numbers.push(i + 1);
+function bestLostNumber () {
+    for (let item of lostNumbers) {
+        if (item%2 ===0) {
+            evenNumbers.push(item);
+        } else if (item%3 ===0) {
+            multNumbers.push(item);
+        }
     }
-    console.log (numbers);
+    console.log(evenNumbers);
+    console.log(multNumbers);
+    const result = concatNumbers.concat(evenNumbers, multNumbers);
+    console.log(result);
+    // concatNumbers = evenNumbers.concat(multNumbers);
+    // console.log(concatNumbers);
 }
-get100Numbers ();
+
+bestLostNumber ();
